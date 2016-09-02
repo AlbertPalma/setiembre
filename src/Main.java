126c57efb595f4ac7697c41671d3c6d23f19a389
Diego Toala <ceusagaintp@hotmail.com>
2016-09-02 08:58:46 -0500

    CAMBIOS CON ERROR

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        String prueba = "Hola";
+        String prueba = "HolaDSGFD"DSFGBDSGHBSDFBEWFHVBSDK VBJDLFKV"R BFGBV";
     }
     
 }
